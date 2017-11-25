f40ee27598fea79d249668e1c3fb4c6ddae42543
Austin Benson <arbenson@gmail.com>
2017-11-24 17:10:44 -0800

    Update universal experiments.

--- README.md
+++ README.md
@@ -4,43 +4,48 @@ This repository accompanies the paper
 
 - Austin R. Benson, Ravi Kumar, and Andrew Tomkins. A Discrete Choice Model for Subset Selection. Proceedings of WSDM, 2018.
 
+## Universal choice sets
 
-## Universal choice datasets
-
-Compute summary statistics of datasets (Table 1 in paper).
+Compute summary statistics of datasets (Table 1 in paper):
 ```
 julia universal_statistics.jl
 ```
 
-Reproduce figures.
+Reproduce figures:
 ```
 julia> include("universal_figures.jl")
 julia> universal_likelihood_gains_plots()  # Figure 1
 julia> negative_corrections_plot()  # Figure 2
 ```
 
-Re-run experiments.
+Re-run experiments (for specific datasets):
 ```
-julia> negative_corrections_experiments()
+julia> include("universal_experiments.jl")
+julia> universal_likelihood_experiments("bakery-5-25")  # data for Figure 1
+julia> negative_corrections_experiment("walmart-items-5-25")  # data for Figure 2
+julia> timing_experiment("kosarak-5-25")  # data for Table 2
+julia> biggest_corrections_experiment("lastfm-genres-5-25")  # data for Table 3
 ```
 
-## Variable choice datasets
+## Variable choice sets
 
 Compute summary statistics of datasets (Table 4 in paper):
 ```
 julia variable_statistics.jl
 ```
 
-Reproduce figures.
+Reproduce figures:
 ```
 julia> include("variable_figures.jl")
 julia> variable_likelihood_gains_plot()
 ```
 
-Re-run experiments.
+Re-run experiments:
 ```
 julia> include("variable_experiments.jl")
 julia> run_variable_model_experiments("yc-cats-5-10-4-8.txt")
 julia> run_variable_model_experiments("yc-items-5-10-4-8.txt")
 ```
+
+## Data
 
--- universal_experiments.jl
+++ universal_experiments.jl
@@ -1,72 +1,13 @@
 include("universal.jl")
 
-function top_choice_tups(data::UniversalChoiceDataset, num::Int64)
+# Get the num most frequent choices.
+function most_freq_choice_tups(data::UniversalChoiceDataset, num::Int64)
     counts = [(count, tup) for (tup, count) in get_subset_counts(data) if length(tup) > 1]
     sort!(counts, rev=true)
     return [collect(tup) for (count, tup) in counts[1:num]]
 end
 
-function negative_corrections_experiments()
-    function run_experiment(basename::AbstractString)
-        println("$basename...")
-        data = read_data("data/$basename.txt")
-        num_updates = min(length(unique(data.choices)), 1000)
-        choices_to_add = top_choice_tups(data, num_updates)
-        model = initialize_model(data)
-        num_negative_corrections = Int64[]
-        for (i, choice) in enumerate(choices_to_add)
-            println(@sprintf("iteration %d of %d", i, num_updates))
-            add_to_hotset!(model, choice)
-            # Get negative corrections
-            count = 0
-            for (subset, val) in model.H
-                separable_prob = prod([model.probs[item] for item in subset])
-                gamma = model.gammas[length(subset)]
-                correction = val - gamma * separable_prob
-                if correction < 0; count += 1; end
-            end
-            push!(num_negative_corrections, count)
-        end
-        
-        output = open("output/$basename-freq-neg-corrections.txt", "w")
-        for (i, num) in enumerate(num_negative_corrections)
-            write(output, @sprintf("%d %d\n", i, num))
-        end
-    end
-
-    run_experiment("bakery-5-25")
-    run_experiment("walmart-depts-5-25")
-    run_experiment("walmart-items-5-25")
-    run_experiment("lastfm-genres-5-25")
-    run_experiment("kosarak-5-25")
-    run_experiment("instacart-5-25")
-end
-
-function biggest_corrections_experiment(data::UniversalChoiceDataset, num_updates::Int64,
-                                        basename::AbstractString)
-    choices_to_add = top_choice_tups(data, num_updates)
-    model = initialize_model(data)
-    num_negative_corrections = Int64[]
-    for choice in choices_to_add; add_to_hotset!(model, choice); end
-
-    data = []
-    for (subset, val) in model.H
-        separable_prob = prod([model.probs[item] for item in subset])
-        gamma = model.gammas[length(subset)]
-        correction = val - gamma * separable_prob
-        push!(data, (correction, subset))
-    end
-
-    sort!(data)
-    output = open("output/$basename-biggest-corrections.txt", "w")
-    for i = 1:5
-        write(output, @sprintf("%d:\t%s\t%f\n", i, join(data[i][2], " "), data[i][1]))
-    end
-    for i = 1:5
-        ind = length(data) - i + 1
-        write(output, @sprintf("%d:\t%s\t%f\n", i, join(data[ind][2], " "), data[ind][1]))
-    end
-end
+maximum_H_size(data::UniversalChoiceDataset) = min(length(unique(data.choices)), 1000)
 
 function universal_improvements(data::UniversalChoiceDataset, num_updates::Int64,
                                 basename::AbstractString, update_type::AbstractString,
@@ -100,7 +41,7 @@ function universal_improvements(data::UniversalChoiceDataset, num_updates::Int64
         for item in choice; item_counts[item] += 1; end
     end
 
-    choices_to_add = top_choice_tups(training_data, num_updates)
+    choices_to_add = most_freq_choice_tups(training_data, num_updates)
     if     update_type == "f"
         # Keep frequency-based updates
     elseif update_type == "nl"
@@ -143,55 +84,105 @@ function universal_improvements(data::UniversalChoiceDataset, num_updates::Int64
         return
     end
 
-    if     update_type == "f";   output = open("output/$basename-single-freq.txt", "w")
-    elseif update_type == "l";   output = open("output/$basename-single-lift.txt", "w")
-    elseif update_type == "nl";  output = open("output/$basename-single-nlift.txt", "w")
+    if     update_type == "f";  output = open("output/$basename-freq.txt", "w")
+    elseif update_type == "l";  output = open("output/$basename-lift.txt", "w")
+    elseif update_type == "nl"; output = open("output/$basename-nlift.txt", "w")
     else   error("Unknown update type")    end
     for i = 0:num_updates
-        write(output, @sprintf("%d %s\n", i, join(log_likelihoods[i + 1, :], " ")))
+        ll = log_likelihoods[i + 1]
+        write(output, "$i $ll\n")
+    end
+    close(output)
+end
+
+function negative_corrections_experiment(basename::AbstractString)
+    println("$basename...")
+    data = read_data("data/$basename.txt")
+    num_updates = maximum_H_size(data)
+    choices_to_add = most_freq_choice_tups(data, num_updates)
+    model = initialize_model(data)
+    num_negative_corrections = Int64[]
+    for (i, choice) in enumerate(choices_to_add)
+        add_to_hotset!(model, choice)
+        # Get negative corrections
+        count = 0
+        for (subset, val) in model.H
+            separable_prob = prod([model.probs[item] for item in subset])
+            gamma = model.gammas[length(subset)]
+            correction = val - gamma * separable_prob
+            if correction < 0; count += 1; end
+        end
+        println("iteration $i of $num_updates: $count corretions < 0")
+        push!(num_negative_corrections, count)
+    end
+    
+    open("output/$basename-freq-neg-corrections.txt", "w") do output
+        for (i, num) in enumerate(num_negative_corrections)
+            write(output, @sprintf("%d %d\n", i, num))
+        end
     end
 end
 
-function timing_experiments()
-    function run_experiment(dataset_file::AbstractString)
-        data = read_data(dataset_file)
-        basename = split(split(dataset_file, "/")[end], ".")[1]
-        num_items = length(unique(data.choices))        
-        num_updates = min(num_items, 10000)
+function biggest_corrections_experiment(basename::AbstractString)
+    data = read_data("data/$basename.txt")
+    num_updates = maximum_H_size(data)    
+    choices_to_add = most_freq_choice_tups(data, num_updates)
+    model = initialize_model(data)
+    num_negative_corrections = Int64[]
+    for choice in choices_to_add; add_to_hotset!(model, choice); end
 
-        # warmup
-        universal_improvements(data, num_updates, basename, "f", true)
-        # run
-        tic()
-        @time universal_improvements(data, num_updates, basename, "f", true)
-        tf = toc()
-        f = open("$basename-times.txt", "w")
+    results = []
+    for (subset, val) in model.H
+        separable_prob = prod([model.probs[item] for item in subset])
+        gamma = model.gammas[length(subset)]
+        correction = val - gamma * separable_prob
+        push!(results, (correction, subset))
+    end
+
+    sort!(results)
+    open("output/$basename-biggest-corrections.txt", "w") do output
+        for i = 1:5
+            line = @sprintf("%d:\t%s\t%f\n", i, join(results[i][2], " "), results[i][1])
+            println(line)
+            write(output, line)
+        end
+        for i = 1:5
+            ind = length(results) - i + 1
+            line = @sprintf("%d:\t%s\t%f\n", i, join(results[ind][2], " "), results[ind][1])
+            println(line)
+            write(output, line)
+        end
+    end
+end
+
+function timing_experiment(basename::AbstractString)
+    data = read_data("data/$basename.txt")
+    num_updates = maximum_H_size(data)    
+    # warmup
+    universal_improvements(data, num_updates, basename, "f", true)
+    # run
+    tic()
+    @time universal_improvements(data, num_updates, basename, "f", true)
+    tf = toc()
+    open("output/$basename-times.txt", "w") do f
         write(f, @sprintf("freq: %f\n", tf))
-        close(f)
     end
-
-    timing_experiment("data/bakery-5-25-clean.txt")
-    timing_experiment("data/walmart-depts-5-25-clean.txt")
-    timing_experiment("data/walmart-items-5-25-clean.txt")
-    timing_experiment("data/lastfm-genres-5-25-clean.txt")
-    timing_experiment("data/kosarak-5-25-clean.txt")
-    timing_experiment("data/instacart-5-25-clean.txt")
 end
 
-function universal_likelihood_improvement_experiments()
-    function run_experiments(basename::AbstractString)
-        data = read_data("data/$basename.txt")
-        num_items = length(unique(data.choices))
-        num_updates = min(length(unique(data.choices)), 1000)
-        universal_improvements(data, num_updates, basename, "f")        
-        universal_improvements(data, num_updates, basename, "nl")
-        universal_improvements(data, num_updates, basename, "l")
-    end
-
-    run_experiments("data/bakery-5-25-clean.txt")
-    run_experiments("data/walmart-depts-5-25-clean.txt")
-    run_experiments("data/walmart-items-5-25-clean.txt")
-    run_experiments("data/lastfm-genres-5-25-clean.txt")
-    run_experiments("data/kosarak-5-25-clean.txt")
-    run_experiments("data/instacart-5-25-clean.txt")
+# Use frequency, lift, and normalized lift heuristics to generate models.
+function universal_likelihood_experiments(basename::AbstractString)
+    println("$basename...")
+    data = read_data("data/$basename.txt")
+    num_updates = min(length(unique(data.choices)), 1000)
+    println("frequency heuristic...")
+    universal_improvements(data, num_updates, basename, "f")
+    println("lift heuristic...")    
+    universal_improvements(data, num_updates, basename, "l")
+    println("normalized lift heuristic...")        
+    universal_improvements(data, num_updates, basename, "nl")
 end
+
+
+# timing_experiment("kosarak-5-25")
+# biggest_corrections_experiment("lastfm-genres-5-25")
+
